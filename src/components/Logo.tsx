4989b53e89516129dd4c4c517b012d1dc0e137ef
Jakethedude123 <jakedkessel@gmail.com>
2025-07-05 19:29:15 -0400

    feat: update site logo to hilogo.png in Logo component

--- src/components/Logo.tsx
+++ src/components/Logo.tsx
@@ -3,7 +3,7 @@ import Image from 'next/image';
 export default function Logo({ className = '', ...props }) {
   return (
     <Image
-      src="/images/logo_transparent.png"
+      src="/hilogo.png"
       alt="CoachCritic Logo"
       width={160}
       height={48}
