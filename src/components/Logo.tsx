06c436a51fd8c4eab3421c41dbb335a6b92d592f
Jakethedude123 <jakedkessel@gmail.com>
2025-07-06 20:54:15 -0400

    fix: use plain img tag for logo to bypass Next.js image optimization issues

--- src/components/Logo.tsx
+++ src/components/Logo.tsx
@@ -1,14 +1,11 @@
-import Image from 'next/image';
-
 export default function Logo({ className = '', ...props }) {
   return (
-    <Image
+    <img
       src="/images/optimized/hilogo.png"
       alt="CoachCritic Logo"
       width={160}
       height={48}
       className={className}
-      priority
       {...props}
     />
   );
