2e4f73e9e83418b5a30f1e157e6b7c12c3b6b5ce
Jakethedude123 <jakedkessel@gmail.com>
2025-07-09 13:50:33 -0400

    fix: remove clientTypes prop from CoachCard in AllCoachesClient to match Coach type

--- src/app/all-coaches/AllCoachesClient.tsx
+++ src/app/all-coaches/AllCoachesClient.tsx
@@ -47,7 +47,6 @@ export default function AllCoachesClient() {
                 rating: 0,
                 testimonialCount: 0,
                 credentials: coach.credentials || [],
-                clientTypes: coach.clientTypes || [],
                 federations: coach.federations || [],
               }} small />
             </li>
